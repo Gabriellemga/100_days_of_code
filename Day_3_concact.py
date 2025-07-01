192c731c20fb33b19a71a0e0050df93a8c419f65
Gabriellemga <gabriellemga@gmail.com>
2025-07-01 16:09:55 -0300

    Create Day_3_concact.py

---
+++ Day_3_concact.py
@@ -0,0 +1,8 @@
+food = input('What is your favorite food?')
+plant = input('what is your favorite plant?')
+cook = input('What is your favorite way to cook?')
+burned_food = input('What word describes burned food?')
+diy = input('Name a DIY item')
+
+print('MENU')
+print(cook, food, 'with', burned_food, plant, 'on a bed of ',diy )
