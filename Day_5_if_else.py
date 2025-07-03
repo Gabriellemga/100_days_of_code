cd3589d6ae1849df74cd2eee247c91955c55b80d
Gabriellemga <gabriellemga@gmail.com>
2025-07-03 18:13:15 -0300

    Create Day_5_if_else.py

---
+++ Day_5_if_else.py
@@ -0,0 +1,18 @@
+print('Marvel Movies Character Creator')
+
+question1 = input('Do you like "hanging around"?:')
+question2 = input('Do you have a "stylish haircut"?:')
+question3 = input('Do you often feel "Marvelous"?:')
+                  
+if question1 == "yes":
+  print('You are Spider-Man! 🕸️')
+else:
+  print("Then you're not Spider-man")
+if question2 == "yes":
+  print("You are Korg")
+else:
+  print("Aww, then you're not Korg")
+if question3 == "yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("Aww... then you're not Captain Marvel")
