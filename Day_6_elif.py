ec34ea0af0a34d42550f1a6ece2e496d9197dd35
Gabriellemga <gabriellemga@gmail.com>
2025-07-05 18:37:05 -0300

    Create Day_6_elif.py

---
+++ Day_6_elif.py
@@ -0,0 +1,17 @@
+print("MY LOGIN SYSTEM")
+print("+" * 15)
+
+username = input("Usenname ->")
+password = input("Password ->")
+
+if username == "Maia" and password == "3a1a":
+  print('''Welcome Maia, what a lovely accent you have, you could have           \n charmed your way in here even without a password.''')
+  print("Have a great day!")
+  print("Don't forget to like and subscribe")
+elif username == "Lola" and password == "1o1a":
+  print("Welcome Lola, May your day be wonderful!")
+elif username == "Kamala" and password == "k4m4l4":
+  print('''HI Kamala, 
+May every second of your day be filled with peace, love and happiness.''')
+else :
+  print('You do not have a passord')
