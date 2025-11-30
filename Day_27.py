b36e670e7fe52ae49f908352c5a3fa652839b886
Gabrielle <gabriellemga@gmail.com>
2025-11-30 18:26:28 -0300

    Update Day_27.py

--- Day_27.py
+++ Day_27.py
@@ -5,17 +5,18 @@ import time
 print('Character Builder\n')
 
 
-side6 = random.randint(1,6)
-side12 = random.randint(1,12)
-
-  
 def character_health():
+  side6 = random.randint(1,6)
+  side12 = random.randint(1,12)
   health = (side6 * side12 / 2) + 10
   return health
   
 def character_strength():
-    strength = (side6 * side12 / 2) + 12
-    return strength
+  side6 = random.randint(1,6)
+  side12 = random.randint(1,12)
+  strength = (side6 * side12 / 2) + 12
+  return strength
+
 
 
 def character_creation():
@@ -31,13 +32,14 @@ def character_creation():
   print('STERNGTH:', character_strength())
   print('\nMay your name go down in Legend...')
   
+   
 
 while True:
   character_creation()
   again = input('\n\nDo you want create another character? (yes/no):')
   if  again == 'yes':
     os.system('clear')
-    character_creation()
+    continue
   else:
      print('Goodbye')
      break
